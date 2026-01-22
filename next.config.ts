88dbd97c950c67a7e624c2b1afef51b5d3f3d697
Abhimanyu Sharma <56254966+Abhisharma08@users.noreply.github.com>
2026-01-22 22:04:51 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,36 @@
-import type { NextConfig } from 'next';
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/ACETECH-hyderabad-2026',
+   basePath: '/ACETECH-hyderabad-2026',
   trailingSlash: true,
-
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   images: {
-    unoptimized: true,
+     unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'www.aluempire.com',
+        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
+        port: '',
         pathname: '/**',
-      },
+      }
     ],
   },
 };
