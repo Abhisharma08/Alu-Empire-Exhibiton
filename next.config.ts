462f48bcf1d91251ebe7dac47b3c905e7ddd4534
Abhimanyu Sharma <56254966+Abhisharma08@users.noreply.github.com>
2026-01-22 21:51:28 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,25 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
+
+const BASE_PATH = '/ACETECH-hyderabad-2026';
+
 const nextConfig: NextConfig = {
-   
-  /* config options here */
-  basePath:'/ACETECH-hyderabad-2026',
+  basePath: BASE_PATH,
+  assetPrefix: BASE_PATH,
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
     ],
   },
 };
